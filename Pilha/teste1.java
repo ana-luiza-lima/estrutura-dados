0f7d3f4856bf03765643693999b8a578ce751e66
Your Name <you@example.com>
2026-09-15 20:09:32 -0300

    Pilhas | Apresentação

---
+++ Pilha/teste1.java
@@ -0,0 +1,25 @@
+public class teste1 {
+
+    static void main(){
+        Pilha<Integer> pilha = new Pilha<>(10);
+
+        System.out.println("A pilha esta vazia: " + pilha.isEmpty());
+
+        pilha.push(10);
+        pilha.push(11);
+        pilha.push(12);
+        pilha.push(13);
+        pilha.push(14);
+
+        System.out.println("A pilha esta vazia: " + pilha.isEmpty());
+
+        pilha.pop();
+        int valor = pilha.pop();
+        System.out.println("Segundo valor desempilhado: " + valor);
+        pilha.pop();
+        pilha.pop();
+        pilha.pop();
+        pilha.pop();
+    }
+    
+}
